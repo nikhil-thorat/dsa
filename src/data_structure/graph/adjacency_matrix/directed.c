b915a8c440d3269f7cfcc105f8eba15c455a2be8
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-05-18 22:07:57 +0530

    done with adjacency matrix for directed and undirected graph

---
+++ src/data_structure/graph/adjacency_matrix/directed.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * DirectedGraph Representation using Adjacency Matrix
+ */
+typedef struct
+{
+    int **matrix;
+    size_t size;
+} UnirectedGraph;
+
+/*
+ * Creates a DirectedGraph and returns
+ * a pointer to it.
+ */
+UnirectedGraph *NewDirectedGraph(size_t size)
+{
+    UnirectedGraph *directed_graph = (UnirectedGraph *)malloc(sizeof(UnirectedGraph));
+    directed_graph->size = size;
+
+    directed_graph->matrix = malloc(directed_graph->size * sizeof(int *));
+    for (int i = 0; i < directed_graph->size; i++)
+    {
+        directed_graph->matrix[i] = malloc(sizeof(int) * directed_graph->size);
+    };
+
+    for (int i = 0; i < directed_graph->size; i++)
+    {
+        for (int j = 0; j < directed_graph->size; j++)
+        {
+            directed_graph->matrix[i][j] = 0;
+        }
+    }
+
+    return directed_graph;
+};
+
+/*
+ * Adds the edge between the source and destination
+ */
+void AddEdge(UnirectedGraph *directed_graph, int src, int dest)
+{
+    directed_graph->matrix[src][dest] = 1;
+};
+
+/*
+ * Removes the edge between the srouce and destination
+ */
+void RemoveEdge(UnirectedGraph *directed_graph, int src, int dest)
+{
+    directed_graph->matrix[src][dest] = 0;
+}
+
+/*
+ * Checks is there is an edge between source and destination
+ */
+int HasEdge(UnirectedGraph *directed_graph, int src, int dest)
+{
+    return directed_graph->matrix[src][dest] == 1;
+}
+
+/*
+ * Prints the directed_graph
+ */
+void Print(UnirectedGraph *directed_graph)
+{
+    for (int i = 0; i < directed_graph->size; i++)
+    {
+        printf("%d : ", i);
+        for (int j = 0; j < directed_graph->size; j++)
+        {
+            printf("%d ", directed_graph->matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    UnirectedGraph *directed_graph = NewDirectedGraph(5);
+    Print(directed_graph);
+
+    AddEdge(directed_graph, 0, 1);
+    AddEdge(directed_graph, 0, 2);
+    AddEdge(directed_graph, 0, 3);
+    AddEdge(directed_graph, 1, 3);
+    AddEdge(directed_graph, 1, 4);
+    AddEdge(directed_graph, 2, 3);
+    AddEdge(directed_graph, 3, 4);
+    Print(directed_graph);
+
+    int has_edge = HasEdge(directed_graph, 1, 3);
+    if (has_edge)
+    {
+        puts("There is an edge between 1 and 3");
+    }
+    else
+    {
+        puts("There is no edge between 1 and 3");
+    }
+
+    RemoveEdge(directed_graph, 1, 3);
+    RemoveEdge(directed_graph, 1, 4);
+    Print(directed_graph);
+
+    has_edge = HasEdge(directed_graph, 1, 4);
+    if (has_edge)
+    {
+        puts("There is an edge between 1 and 4");
+    }
+    else
+    {
+        puts("There is no edge between 1 and 4");
+    }
+
+    return 0;
+}
---
+++ src/data_structure/graph/adjacency_matrix/undirected.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * UndirectedGraph Representation using Adjacency Matrix
+ */
+typedef struct
+{
+    int **matrix;
+    size_t size;
+} UndirectedGraph;
+
+/*
+ * Creates a UndirectedGraph and returns
+ * a pointer to it.
+ */
+UndirectedGraph *NewDirectedGraph(size_t size)
+{
+    UndirectedGraph *undirected_graph = (UndirectedGraph *)malloc(sizeof(UndirectedGraph));
+    undirected_graph->size = size;
+
+    undirected_graph->matrix = malloc(undirected_graph->size * sizeof(int *));
+    for (int i = 0; i < undirected_graph->size; i++)
+    {
+        undirected_graph->matrix[i] = malloc(sizeof(int) * undirected_graph->size);
+    };
+
+    for (int i = 0; i < undirected_graph->size; i++)
+    {
+        for (int j = 0; j < undirected_graph->size; j++)
+        {
+            undirected_graph->matrix[i][j] = 0;
+        }
+    }
+
+    return undirected_graph;
+};
+
+/*
+ * Adds the edge between the source and destination
+ */
+void AddEdge(UndirectedGraph *undirected_graph, int src, int dest)
+{
+    undirected_graph->matrix[src][dest] = 1;
+    undirected_graph->matrix[dest][src] = 1;
+};
+
+/*
+ * Removes the edge between the srouce and destination
+ */
+void RemoveEdge(UndirectedGraph *undirected_graph, int src, int dest)
+{
+    undirected_graph->matrix[src][dest] = 0;
+    undirected_graph->matrix[dest][src] = 0;
+}
+
+/*
+ * Checks is there is an edge between source and destination
+ */
+int HasEdge(UndirectedGraph *undirected_graph, int src, int dest)
+{
+    return undirected_graph->matrix[src][dest] == 1;
+}
+
+/*
+ * Prints the directed_graph
+ */
+void Print(UndirectedGraph *undirected_graph)
+{
+    for (int i = 0; i < undirected_graph->size; i++)
+    {
+        printf("%d : ", i);
+        for (int j = 0; j < undirected_graph->size; j++)
+        {
+            printf("%d ", undirected_graph->matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    UndirectedGraph *undirected_graph = NewDirectedGraph(5);
+    Print(undirected_graph);
+
+    AddEdge(undirected_graph, 0, 1);
+    AddEdge(undirected_graph, 0, 2);
+    AddEdge(undirected_graph, 0, 3);
+    AddEdge(undirected_graph, 1, 3);
+    AddEdge(undirected_graph, 1, 4);
+    AddEdge(undirected_graph, 2, 3);
+    AddEdge(undirected_graph, 3, 4);
+    Print(undirected_graph);
+
+    int has_edge = HasEdge(undirected_graph, 1, 3);
+    if (has_edge)
+    {
+        puts("There is an edge between 1 and 3");
+    }
+    else
+    {
+        puts("There is no edge between 1 and 3");
+    }
+
+    RemoveEdge(undirected_graph, 1, 3);
+    RemoveEdge(undirected_graph, 1, 4);
+    Print(undirected_graph);
+
+    has_edge = HasEdge(undirected_graph, 1, 4);
+    if (has_edge)
+    {
+        puts("There is an edge between 1 and 4");
+    }
+    else
+    {
+        puts("There is no edge between 1 and 4");
+    }
+
+    return 0;
+}
