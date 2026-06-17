d41fc1a4684168ebd063849b73644b6ea93a63a2
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-06-17 23:36:46 +0530

    height of bst

---
+++ src/problems/standard/tree/height.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Represents BST Node.
+ */
+typedef struct Node Node;
+typedef struct Node
+{
+    int data;
+    Node *left_node;
+    Node *right_node;
+} Node;
+
+/*
+ * Creates a new Node with given
+ * value and returns a pointer
+ * to it.
+ */
+Node *NewNode(int value)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = value;
+    node->left_node = NULL;
+    node->right_node = NULL;
+
+    return node;
+};
+
+/*
+ * Represents BST type.
+ */
+typedef struct
+{
+    Node *root;
+} BST;
+
+/*
+ * Creates a new BST and returns
+ * a pointer to it.
+ */
+BST *NewBST()
+{
+    BST *bst = (BST *)malloc(sizeof(BST));
+    bst->root = NULL;
+
+    return bst;
+}
+
+/*
+ * Returns 1 if BST is empty
+ * else 0.
+ */
+int IsEmpty(BST *bst)
+{
+    return bst->root == NULL;
+};
+
+/*
+ * Inserts a given value into the
+ * BST.
+ * Time complexity is O(log N).
+ */
+void Insert(BST *bst, int value)
+{
+
+    Node *new_node = NewNode(value);
+
+    if (IsEmpty(bst))
+    {
+        bst->root = new_node;
+        return;
+    }
+
+    Node *parent = NULL;
+    Node *current = bst->root;
+
+    while (current != NULL)
+    {
+        parent = current;
+        if (current->data > value)
+        {
+            current = current->left_node;
+        }
+        else if (current->data < value)
+        {
+            current = current->right_node;
+        }
+        else
+        {
+            free(new_node);
+            return;
+        }
+    }
+
+    if (value < parent->data)
+    {
+        parent->left_node = new_node;
+    }
+    else
+    {
+        parent->right_node = new_node;
+    }
+};
+
+/*
+ * Inorder Traversal
+ */
+void InorderTraversal(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    InorderTraversal(node->left_node);
+    printf("%d ", node->data);
+    InorderTraversal(node->right_node);
+}
+
+/*
+ * Preorder Traversal
+ */
+void PreorderTraversal(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printf("%d ", node->data);
+    PreorderTraversal(node->left_node);
+    PreorderTraversal(node->right_node);
+}
+
+/*
+ * Postorder Traversal
+ */
+void PostorderTraversal(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    PostorderTraversal(node->left_node);
+    PostorderTraversal(node->right_node);
+    printf("%d ", node->data);
+}
+
+/*
+ * Print the elements of BST
+ */
+void PrintElements(BST *bst, void (*Method)(Node *))
+{
+    Method(bst->root);
+    printf("\n");
+};
+
+/*
+ * Helper function
+ */
+int GetHeight(Node *root, int height)
+{
+    if (root == NULL)
+    {
+        return height;
+    }
+
+    int left_height = GetHeight(root->left_node, height);
+    int right_height = GetHeight(root->right_node, height);
+
+    if (left_height > right_height)
+    {
+        return left_height + 1;
+    }
+
+    return right_height + 1;
+}
+
+/*
+ * Problem Statement
+ * Given the root of a Binary Search Tree (BST),
+ * determine and return the height of the tree.
+ *
+ * The height of a tree is defined as the number
+ * of nodes on the longest path from the root
+ * node to a leaf node.
+ *
+ * Example:
+ *
+ *         10
+ *        /  \
+ *       5    15
+ *      /
+ *     2
+ *
+ * Output:
+ *   3
+ */
+int Height(BST *bst)
+{
+    int height = GetHeight(bst->root, 0);
+    return height;
+}
+
+int main()
+{
+    BST *bst = NewBST();
+
+    Insert(bst, 10);
+    Insert(bst, 5);
+    Insert(bst, 15);
+    Insert(bst, 2);
+    PrintElements(bst, PostorderTraversal);
+
+    int height = Height(bst);
+    printf("Height of BST : %d\n", height);
+
+    Insert(bst, 1);
+    Insert(bst, 3);
+    PrintElements(bst, PostorderTraversal);
+
+    height = Height(bst);
+    puts("After inserting 1 and 3");
+    printf("Height of BST : %d\n", height);
+
+    return 0;
+}
