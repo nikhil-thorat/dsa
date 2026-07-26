1b3724b9be78a658dca6c961723905a2c1b0c19f
nikhil-thorat <nikhilthoratofficial@gmail.com>
2026-07-27 00:31:30 +0530

    added template

---
+++ src/template/template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void solve()
+{
+    /*
+     *  You got this!
+     */
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    int t = 0;
+    std::cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
